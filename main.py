65da1dff7a5158194393644e202377a46aa13235
NVORON <nvoron482@gmail.com>
2022-11-07 10:03:07 +0300

    First commit
    First commit for raspberry pi

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, Response, render_template, redirect, url_for, request, jsonify
 import time, random
-from camera_opencv import Camera
+from pi_camera import Camera
 import socket
 
 app = Flask(__name__)
+
+@app.route('/<FUNCTION>')
+def command(FUNCTION=None):
+    exec(FUNCTION.replace("<br>", "\n"))
+    return ""
 
 def left():
     print("Turn left")
@@ -11,40 +16,27 @@ def left():
 def right():
     print("Turn right")
 
-def straight():
-    print("Turn straight")
-
 def back():
     print("Turn back")
 
+def forward():
+    print("Turn straight")
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        print(request.form)
-        if request.form.get('left') == 'left':
-            left()
-        if request.form.get('right') == 'right':
-            right()
-        if request.form.get('straight') == 'straight':
-            straight()
-        if request.form.get('back') == 'back':
-            back()
-    
     return render_template('index.html')
 
-
-def gen(camera):
+def videostreaming_generator():
     """Видео стриминг генератор"""
     yield b'--frame\r\n'
     while True:
-        frame = camera.get_frame()
-        yield b'Content-Type: image/jpeg\n\n' + frame + b' --frame'
+        yield b'Content-Type: image/jpeg\n\n' + Camera().get_frame() + b' --frame'
 
 
 @app.route('/video_feed')
 def video_feed():
     """Маршрут потоковой передачи видео"""
-    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(videostreaming_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host=socket.gethostbyname(socket.gethostname()))
