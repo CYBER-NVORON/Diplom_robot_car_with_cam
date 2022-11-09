0abdcacc31fa789eeec97e2ffee623e9812f5476
NVORON <nvoron482@gmail.com>
2022-11-10 02:53:55 +0300

    rebase for pi

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 from flask import Flask, Response, render_template, redirect, url_for, request, jsonify
 import time, random
-from pi_camera import Camera
+from AlphaBot import AlphaBot
+from camera_opencv import Camera
 import socket
 
 app = Flask(__name__)
 
 @app.route('/<FUNCTION>')
 def command(FUNCTION=None):
+    car.setPWMA(float(speed))
+    car.setPWMB(float(speed))
     exec(FUNCTION.replace("<br>", "\n"))
     return ""
 
 def left():
+    car.left()
     print("Turn left")
 
 def right():
+    car.right()
     print("Turn right")
 
 def back():
+    car.backward()
     print("Turn back")
 
 def forward():
+    car.forward()
     print("Turn straight")
+
+def stop():
+    car.stop()
+    print("Stop")
+
+def left_servo():
+    print("Turn left")
+
+def right_servo():
+    print("Turn right")
+
+def down_servo():
+    print("Turn back")
+
+def up_servo():
+    print("Turn straight")
+
+def stop_servo():
+    print("Stop")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -39,4 +65,10 @@ def video_feed():
     return Response(videostreaming_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True, host=socket.gethostbyname(socket.gethostname()))
+    car = AlphaBot()
+    speed = 50
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    ip=s.getsockname()[0]
+    s.close()
+    app.run(debug=True, host=ip)
